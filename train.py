45a991b2d2ff839eff1ebce67d2d815952ad83f6
Bohdan <bogdan.magometa@gmail.com>
2023-12-03 01:38:22 +0200

    wandb

--- train.py
+++ train.py
@@ -14,6 +14,7 @@ from torch.utils.data import Subset, DataLoader
 import numpy as np
 import pandas as pd
 from PIL import Image
+import wandb
 
 from data import SportDataset
 from utils import temp_seed
@@ -49,6 +50,13 @@ def get_dataset():
 NUM_EPOCHS = 100
 
 if __name__ == "__main__":
+    config = {
+        'batch_size': 64,
+        'val_batch_size': 256,
+        'model': 'efficientnet',
+    }
+    
+    
     # Train dataset
     dataset = get_dataset()
 
@@ -57,11 +65,11 @@ if __name__ == "__main__":
         train_dataset, val_dataset = torch.utils.data.random_split(dataset, [0.9, 0.1])
 
     # Loaders
-    train_loader = DataLoader(train_dataset, batch_size=1, shuffle=True, drop_last=True, pin_memory=True)
-    val_loader = DataLoader(val_dataset, batch_size=16, shuffle=False, drop_last=False, pin_memory=True)
+    train_loader = DataLoader(train_dataset, batch_size=config['batch_size'], num_workers=10, shuffle=True, drop_last=True, pin_memory=True)
+    val_loader = DataLoader(val_dataset, batch_size=config['val_batch_size'], num_workers=10, shuffle=False, drop_last=False, pin_memory=True)
 
     # model = models.alexnet(weights=models.AlexNet_Weights.IMAGENET1K_V1)
-    # in_features = model.classifier[6].weight.shape[1]
+    # in_features = model.classifier[6].in_features
     # model.classifier[6] = nn.Linear(in_features, SportDataset.NUM_CLASSES)
     
     # model = models.vit_h_14(weights=models.ViT_H_14_Weights.IMAGENET1K_SWAG_LINEAR_V1)
@@ -75,18 +83,38 @@ if __name__ == "__main__":
     model.cuda();
 
     NUM_EPOCHS = 100
-    optimizer = torch.optim.AdamW(model.parameters())
+    optimizer = torch.optim.Adam(model.parameters())
+
+    wandb.init(
+        entity="bohdanmahometa",
+        project="sports-classification",
+        resume='never',
+        config=config
+    )
+    config = wandb.run.config
+    wandb.run.log_code('.')
 
     val_loss, val_acc = val_step(model, val_loader)
     print(f"Preliminary val:\t{val_loss:.2f} loss \t {val_acc:.3f} acc\n")
+    wandb.log({
+        'val_loss': val_loss,
+        'val_acc': val_acc,
+        'epoch': 0
+    })
 
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=2, threshold=0.01)
 
-    for epoch in range(NUM_EPOCHS):
+    for epoch in range(1, NUM_EPOCHS + 1):
         lr = optimizer.param_groups[0]['lr']
         print(f"lr = {lr}")
         if lr < 1e-6:
             break
+
+        # Log LR
+        print(f"lr = {lr}")
+        wandb.log({
+            'lr': lr
+        }, commit=False)
 
         model.train()
         train_loss = torch.tensor(0.0, dtype=torch.float32, device='cuda')
@@ -105,10 +133,24 @@ if __name__ == "__main__":
                 train_loss += loss * len(imgs)
                 train_correct += torch.sum(torch.argmax(logits, axis=1) == labels)
 
-        print(f"Train epoch {epoch}:\t{train_loss.item() / train_count:.2f} loss \t {train_correct.item() / train_count:.3f} acc")
+        # Log train metrics
+        train_loss = train_loss.item() / train_count
+        train_acc = train_correct.item() / train_count
+        print(f"Train epoch {epoch}:\t{train_loss:.2f} loss \t {train_acc:.3f} acc")
+        wandb.log({
+            'train_loss': train_loss,
+            'train_acc': train_acc,
+            'epoch': epoch
+        }, commit=False)
 
+        # Log val metrics
         val_loss, val_acc = val_step(model, val_loader)
         print(f"Val epoch {epoch}:\t{val_loss:.2f} loss \t {val_acc:.3f} acc\n")
+        wandb.log({
+            'val_loss': val_loss,
+            'val_acc': val_acc
+        })
 
         scheduler.step(val_loss)
 
+    wandb.finish()
