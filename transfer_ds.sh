2204ff483b29619330d4203d9cbd63b6f844d83b
Bohdan <bogdan.magometa@gmail.com>
2023-12-02 18:08:26 +0200

    tran

--- transfer_ds.sh
+++ transfer_ds.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -r ./hw-ucu-12023-4-100-sports-image-classification brook2:/titan/bohdan/sports-classification/hw-ucu-12023-4-100-sports-image-classification
+rsync --progress -r ./hw-ucu-12023-4-100-sports-image-classification.zip brook2:/titan/bohdan/sports-classification/
